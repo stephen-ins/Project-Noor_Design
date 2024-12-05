35a119de36911626114e0db925a37819ba20f246
stephen.insixiengmay <stephen.insixiengmay@gmail.com>
2024-12-05 20:40:58 +0100

    first commit

---
+++ scripts/pages/index.js
@@ -0,0 +1,4 @@
+import {  } from "../utils/Api.js";
+import { Header } from "../components/Header.js";
+import { Main } from "../components/Main.js";
+import { Footer } from "../components/Footer.js";
